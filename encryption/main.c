a59616bbf59e7d4450b40185a780575365f942d8
Joseph Alfredo <Jalfredopoly@gmail.com>
2017-11-08 09:58:23 -0800

    fix Mclk pin

--- encryption/main.c
+++ encryption/main.c
@@ -44,10 +44,10 @@ void main(void) {
     P2->SEL0 &= ~BIT1;
     P2->DIR |= BIT1;
 
-    P4->SEL1 &= ~BIT2;
-    P4->SEL0 &= ~BIT2;
+    P4->SEL0 |= BIT3;
+    P4->SEL1 &= ~BIT3;
     P4->DIR |= BIT3;
-    P4->OUT &= ~BIT3;
+
 
     set_DCO(freq);
 
