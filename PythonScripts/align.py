f9c09c327cebf971f8c3caf8005f7438c575d30a
Kevin <keviny1217@gmail.com>
2018-01-29 08:31:25 -0800

    Starting work on error function

--- PythonScripts/align.py
+++ PythonScripts/align.py
@@ -5,6 +5,17 @@ from scipy import signal
 from sys import argv
 from matplotlib import pyplot as plt
 
+def errorFun(wave1, wave2):
+	error = []
+
+	for shift in range(-len(wave1) + 1, len(wave1)):
+		error[shift] = 0
+		for i, j in zip(range(len(wave1)), range(len(wave2) + shift)):
+			if(j < len(wave2) or j < 0):
+				error[shift] += (wave1[i] - wave2[j]) ** 2
+
+	return error
+
 def main(argv):
 	wave_data = pd.read_csv(argv[1])
 	x = wave_data["time"]
@@ -14,7 +25,8 @@ def main(argv):
 	#plt.plot(x, y1, x, y2)
 	
 	dx = np.mean(np.diff(x))
-	shift = (np.argmax(signal.correlate(y1, y2)) - len(y1)) * dx
+	# shift = (np.argmax(signal.correlate(y1, y2)) - len(y1)) * dx
+	shift = (np.argmin(errorFun(y1, y2)))
 	plt.plot(x, y1, x + shift, y2)
 	plt.show()
 	
