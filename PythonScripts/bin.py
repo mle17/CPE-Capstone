15d74ae947d02bb29e880031a5666b228e0b86d5
Kevin <keviny1217@gmail.com>
2018-03-10 10:53:27 -0800

    Loop through files

--- PythonScripts/bin.py
+++ PythonScripts/bin.py
@@ -1,11 +1,17 @@
-import numpy as np
-import pandas as pd
 import sys
 import struct
 
+BIT = '1'
+
 def main():
-   counter = 0
-   file = pd.read_csv('')
+   for counter in range(0, 51 if BIT == '0' else 41):
+      filename = 'Bit' + BIT + '/scope_' + str(counter) + '.csv'
+      print(filename)
+   # Loop through files
+      # Open current file
+      # Loop through csv
+         # Check when trigger is high, store in array
+      # Output to another csv
 
 if __name__== "__main__":
     main()
