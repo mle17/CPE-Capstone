7908689a486a39d22a9651148d09bc2e7f9fba0c
Joseph Alfredo <jalfredo@calpoly.edu>
2018-03-10 11:36:59 -0800

    Functional bit data sorting

--- PythonScripts/bin.py
+++ PythonScripts/bin.py
@@ -1,7 +1,11 @@
 import sys
 import struct
+import pandas as pd
+import numpy as np
+import csv
 
 BIT = '1'
+TRIG_VOLT = 3
 
 def main():
    for counter in range(0, 51 if BIT == '0' else 41):
@@ -13,5 +17,28 @@ def main():
          # Check when trigger is high, store in array
       # Output to another csv
 
+def add_bit_data_to_csv(source_file):
+    src_csv = pd.read_csv(source_file)
+    result_csv = open('outputBit' + BIT + '.csv', 'w')
+    wr = csv.writer(result_csv, delimiter=',')
+
+    src_vout = list(src_csv["1"])[1:]
+    src_trigger = list(src_csv["2"])[1:]
+    src_vout = [float(i) for i in src_vout]
+    src_trigger = [float(i) for i in src_trigger]
+
+    bit_data = []
+    is_triggered = False
+    for vout_data, trigger_data in zip(src_vout, src_trigger):
+        if not is_triggered and trigger_data > TRIG_VOLT:
+            is_triggered = True
+        elif is_triggered and trigger_data < TRIG_VOLT:
+            is_triggered = False
+            wr.writerows([bit_data])
+            bit_data = []
+
+        if is_triggered:
+            bit_data.append(vout_data)
+
 if __name__== "__main__":
-    main()
+    add_bit_data_to_csv("Bit0/scope_0.csv")
