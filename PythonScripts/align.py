357fbb88861dad11705e68da4a116b605f8cb478
Kevin <keviny1217@gmail.com>
2018-03-14 14:50:28 -0700

    Comments for the python scripts

--- PythonScripts/align.py
+++ PythonScripts/align.py
@@ -6,12 +6,14 @@ from scipy import signal
 from sys import argv
 from matplotlib import pyplot as plt
 
-# Only works for similar waveforms and only gets kinda close
+# Only works for similar waveforms and only approximates
 def libFun(wave1, wave2):
 	mid = len(wave1)-1
 	cor = np.correlate(wave1, wave2, 'full')
+	print(cor, signal.correlate(wave1, wave2, 'full'))
 	return np.argmax(cor) - mid
 
+# Gets more accurate alignment but much more time consuming
 def errorFun(wave1, wave2):
 	wave_len = len(wave1)
 
@@ -49,20 +51,16 @@ def errorFun(wave1, wave2):
 
 	return min_shift
 
+# Align two waveforms
 def main(argv):
 	wave_data = pd.read_csv(argv[1])
 	x = wave_data["time"]
 	y1 = wave_data["base sinewave"]
 	y2 = wave_data["base cosine"]
 
-	#plt.plot(x, y1, x, y2)
-
 	dx = np.mean(np.diff(x))
-	# shift = (np.argmax(signal.correlate(y1, y2)) - len(y1)) * dx
 	plt.plot(x, y1, x, y2)
-	# shift = errorFun(y1, y2) * dx
 	shift = libFun(y1, y2) * dx
-	print(shift)
 	plt.plot(x, y1, x + shift, y2)
 	plt.show()
 
--- PythonScripts/bin.py
+++ PythonScripts/bin.py
@@ -5,21 +5,33 @@ import numpy as np
 import csv
 
 BIT = '1'
-TRIG_VOLT = 3
-BIT_1_THRESH = 100
+KEY = '1'
+TRIG_VOLT = 3        # Cutoff voltage for a trigger high
+BIT_1_THRESH = 100   # Cutoff threshold for number of points in a 0 bit model
 
+# Converts waveform captured from each trigger of the encryption cycle 
+# into their own .csv to be later converted into a model
 def main():
-   result_csv = open('outputBit' + BIT + '.csv', 'w')
-   delay_csv = open('delayBit' + BIT + '.csv', 'w')
+   if KEY == '1':
+      result_csv = open('outputKey.csv', 'w')
+   else:
+      result_csv = open('outputBit' + BIT + '.csv', 'w')
+      # Data for the delays are captured as well
+      delay_csv = open('delayBit' + BIT + '.csv', 'w')
 
-   if BIT == '0':
+   if KEY == '1':
+      # Key model
+      for counter in range(31, 61):
+         filename = 'Key/scope_' + str(counter) + '.csv'
+         add_bit_data_to_csv(filename, result_csv, None)
+   elif BIT == '0':
+      # Bit 0 model
       for counter in range(41, 91):
-         print(counter)
          filename = 'Bit' + BIT + '/scope_' + str(counter) + '.csv'
          add_bit_data_to_csv(filename, result_csv, delay_csv)
    else:
+      # Bit 1 model
       for counter in range(0, 41):
-         print(counter)
          filename = 'Bit' + BIT + '/scope_' + str(counter) + '.csv'
          add_bit_data_to_csv(filename, result_csv, delay_csv)
 
@@ -27,37 +39,43 @@ def main():
    delay_csv.close()
 
 def add_bit_data_to_csv(source_file, result_csv, delay_csv):
-    src_csv = pd.read_csv(source_file)
-    wr_result = csv.writer(result_csv, delimiter=',')
-    wr_delay = csv.writer(delay_csv, delimiter=',')
+   src_csv = pd.read_csv(source_file)
+   wr_result = csv.writer(result_csv, delimiter=',')
+   if delay_csv:
+   wr_delay = csv.writer(delay_csv, delimiter=',')
 
-    src_vout = list(src_csv["1"])[1:]
-    src_trigger = list(src_csv["2"])[1:]
-    src_vout = [float(i) for i in src_vout]
-    src_trigger = [float(i) for i in src_trigger]
+   src_vout = list(src_csv["1"])[1:]
+   src_trigger = list(src_csv["2"])[1:]
+   src_vout = [float(i) for i in src_vout]
+   src_trigger = [float(i) for i in src_trigger]
 
-    bit_data = []
-    delay_data = []
-    is_triggered = False
-    for vout_data, trigger_data in zip(src_vout, src_trigger):
-        if not is_triggered and trigger_data > TRIG_VOLT:
-            is_triggered = True
+   bit_data = []
+   delay_data = []
+   is_triggered = False
+   for vout_data, trigger_data in zip(src_vout, src_trigger):
+      # Start of a trigger
+      if not is_triggered and trigger_data > TRIG_VOLT:
+         is_triggered = True
+         if KEY != '1':
             wr_delay.writerows([delay_data])
             delay_data = []
-        elif is_triggered and trigger_data < TRIG_VOLT:
-            is_triggered = False
-            if BIT == '1':
-                if len(bit_data) > BIT_1_THRESH:
-                    wr_result.writerows([bit_data])
-            else:
-                if len(bit_data) < BIT_1_THRESH:
-                    wr_result.writerows([bit_data])
-            bit_data = []
+      # End of a trigger
+      elif is_triggered and trigger_data < TRIG_VOLT:
+         is_triggered = False
+         if BIT == '1' or KEY == '1':
+               if len(bit_data) > BIT_1_THRESH:
+                  wr_result.writerows([bit_data])
+         else:
+               if len(bit_data) < BIT_1_THRESH:
+                  wr_result.writerows([bit_data])
+         bit_data = []
 
-        if is_triggered:
-            bit_data.append(vout_data)
-        else:
-            delay_data.append(vout_data)
+      # Trigger high means data for the bit
+      if is_triggered:
+         bit_data.append(vout_data)
+      # Trigger low means data for the delay
+      else:
+         delay_data.append(vout_data)
 
 if __name__== "__main__":
     main()
